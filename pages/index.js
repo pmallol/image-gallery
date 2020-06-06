71320da960b4ec4d21ad425ed4087902450c7fd2
pmallol <pmallol@manas.tech>
2020-06-06 16:17:16 -0300

    Show image details and functionality in Lightbox

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,17 @@ import Lightbox from 'react-image-lightbox'
 const Landing = () => {
   const [images, setImages] = useState(null)
   const [imageIndex, setImageIndex] = useState(null)
+  const [imageDetails, setImageDetails] = useState(null)
   const [openLightbox, setOpenLightbox] = useState(false)
 
   const authPath = "http://interview.agileengine.com/auth"
   const getImages = "http://interview.agileengine.com/images"
+  const getImage = "http://interview.agileengine.com/images"
+  
+  const selectImage = (id) => () => {
+    setOpenLightbox(true)
+    setImageIndex(id)
+  }
 
   useEffect(() => {
     fetch(authPath, {
@@ -31,6 +38,31 @@ const Landing = () => {
     })
   }, [])
 
+  useEffect(() => {
+    if (images && imageIndex !== null) {
+      // console.log(images, imageIndex)
+      fetch(authPath, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+          { apiKey: process.env.AE_API_KEY }
+        )
+      })
+      .then(response => response.json())
+      .then((body) => {
+        fetch(`${getImage}/${images.pictures[imageIndex].id}`, {
+          headers: {
+            'Authorization': `Bearer ${body.token}`
+          }
+        })
+        .then(res => res.json())
+        .then(body => setImageDetails(body))
+      })
+    }
+  }, [images, imageIndex])
+
   if (!images) {
     return "No images"
   }
@@ -43,10 +75,7 @@ const Landing = () => {
           <a 
             href="#" 
             key={image.id} 
-            onClick={() => {
-              setOpenLightbox(true)
-              setImageIndex(id)
-            }}
+            onClick={selectImage(id)}
           >
             <img src={image.cropped_picture} />
           </a>
@@ -55,8 +84,13 @@ const Landing = () => {
     </div>
     {openLightbox && (
       <Lightbox 
-        mainSrc={images.pictures[imageIndex].cropped_picture}
+        mainSrc={imageDetails ? imageDetails.full_picture : images.pictures[imageIndex].cropped_picture}
+        prevSrc={images.pictures[(imageIndex + images.pictures.length - 1) % images.pictures.length]}
+        nextSrc={images.pictures[(imageIndex + 1) % images.pictures.length]}
         onCloseRequest={() => setOpenLightbox(false)}
+        onMovePrevRequest={() => setImageIndex((imageIndex + images.pictures.length - 1) % images.pictures.length)}
+        onMoveNextRequest={() => setImageIndex((imageIndex + 1) % images.pictures.length)}
+        imageCaption={imageDetails ? `${imageDetails.author} | ${imageDetails.camera} | ${imageDetails.tags}`  : ""}
       />
     )}
 
